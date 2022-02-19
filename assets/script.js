5e54229e9cdb19e2d59ad00000cdd7bbbeeccceb
Maddy <madisonschaaf48@gmail.com>
2022-02-18 17:13:58 -0700

    added bootstrap classes to align time zones and added current time

---
+++ assets/script.js
@@ -0,0 +1,46 @@
+
+
+//todays time and date
+var utahTime= document.getElementById("utahTime");
+var currentTime = moment();
+utahTime.textContent = currentTime.format("dddd, MMMM Do YYYY, h:mm:ss:a");
+
+
+
+
+//var createTask = function(taskText, taskDate, taskList) {
+    //create elements that make up a task
+   // var taskLi = $("<li>").addClass("list-group-item");
+    //var taskSpan = $("<span>")
+    //.addCLass()
+//}
+
+
+
+//once the page loads this ufnction will happen
+//$(document).ready(function {
+//})
+
+//drag and drop featuer
+//$(".column").sortable({
+  //  connectWith: $(".container .card"),
+    //scroll: false,
+//    tolerance: "pointer",
+  //  helper: "clone",
+   // activate: function(event) {
+    //  console.log("activate", this);
+    //},
+   // deactivate: function(event) {
+   //   console.log("deactivate", this);
+   // },
+   // over: function(event) {
+    //  console.log("over", event.target);
+   // },
+    //out: function(event) {
+   //   console.log("out", event.target);
+   // },
+   // update: function(event) {
+   //   console.log($(this).children());
+   // }
+ // });
+  
