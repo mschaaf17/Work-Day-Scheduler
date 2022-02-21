e97409ceb413b295f7f86bec5423dd6cc839c758
Maddy <madisonschaaf48@gmail.com>
2022-02-20 19:15:27 -0700

    timeblocks set up with different classes

--- assets/script.js
+++ assets/script.js
@@ -1,87 +1,57 @@
-
-
 //todays time and date
-var utahTime= document.getElementById("utahTime");
-var currentTime = moment();
-utahTime.textContent = currentTime.format("dddd, MMMM Do YYYY, h:mm:a");
-var storage = JSON.parse(localStorage.getItem("reminders")) || {};
-console.log(storage)
+var timeOfDay = function () {
+  var utahTime = document.getElementById("utahTime");
+  var currentTime = moment();
+  utahTime.textContent = currentTime.format("dddd, MMMM Do YYYY, h:mm a");
+  setInterval(function() {timeOfDay(); }, 60000)
+}
+timeOfDay();
 
-var midnight = document.querySelector("#x1");
+var storage = JSON.parse(localStorage.getItem("reminders")) || {};
 
 //create a object loop that will take the key and assign the value of the key to the corresponding id of the textarea
-for(var [key, value] of Object.entries(storage)) {
-  console.log(key, value);
-  //get element by id pass in the key then assign the value to each key so they wont override each other matches 
-  //create textarea x1 x2 
+for (var [key, value] of Object.entries(storage)) {
   var textArea = document.getElementById(key)
   localStorage.getItem(key)
   textArea.innerHTML = value
-  console.log(value)
-    
 }
+
+//this tells the hour of the day
+$('.time-block').each(function () {
+  var time = $(this).data('time')
+  console.log(time)
+var current = moment().hours()
+console.log(current)
+
+if(current < time) {
+  $(this).children('.reminders').addClass('future')
+}
+else if (current > time) {
+  $(this).children('.reminders').addClass('past')
+}
+
+else if (current === time) {
+  $(this).children('.reminders').addClass('present')
+}
+
+})
+
 
 //event listener for saving button on reminders
-document.querySelector(".container-fluid").addEventListener("click", function(event) {
-  if(event.target.matches("button") === false) {
-  return
-}
+$('.saveBtn').each(function(){
+  $(this).on('click', function(event) {
 
-var id = "x" + event.target.id
-  var reminders = document.getElementById(id).value; 
+  var id = "x" + event.target.id
+  var reminders = $('#'+ id).val();
   storage[id] = reminders
   localStorage.setItem("reminders", JSON.stringify(storage));
-  console.log("reminder was saved") 
- 
+  console.log("reminder was saved")
+})
 });
 function getValue() {
   var storedText = localStorage.getItem("reminders");
-  if(storedText != null) {
-    
+  if (storedText != null) {
+
   }
 }
 
-
-
-
-
-
-//for loop to save all the text areas!!!!!
-//for(i=0; i < reminders.length; i++) {}
-
-//var createTask = function(taskText, taskDate, taskList) {
-    //create elements that make up a task
-   // var taskLi = $("<li>").addClass("list-group-item");
-    //var taskSpan = $("<span>")
-    //.addCLass()
-//}
-
-
-
-//once the page loads this ufnction will happen
-//$(document).ready(function {
-//})
-
-//drag and drop featuer
-//$(".column").sortable({
-  //  connectWith: $(".container .card"),
-    //scroll: false,
-//    tolerance: "pointer",
-  //  helper: "clone",
-   // activate: function(event) {
-    //  console.log("activate", this);
-    //},
-   // deactivate: function(event) {
-   //   console.log("deactivate", this);
-   // },
-   // over: function(event) {
-    //  console.log("over", event.target);
-   // },
-    //out: function(event) {
-   //   console.log("out", event.target);
-   // },
-   // update: function(event) {
-   //   console.log($(this).children());
-   // }
- // });
-  
